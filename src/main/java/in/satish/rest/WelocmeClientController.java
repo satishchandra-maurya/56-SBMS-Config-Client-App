df66566a80ce46c80e7b0980dbd5b8fce63e9351
SATISH MAURYA <satishchandra-maurya>
2022-08-25 07:31:02 -0700

    reading data from client App using config server

---
+++ src/main/java/in/satish/rest/WelocmeClientController.java
@@ -0,0 +1,21 @@
+package in.satish.rest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class WelocmeClientController {
+
+	@Value("${msg:Confgi Server Not Responding}")
+	private String msg;
+	
+	@GetMapping("/")
+	public String getWelcomeMsg() {
+		System.out.println(msg);
+		return msg;
+	}
+	
+}
